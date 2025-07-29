34aae2b73d71be3eec2e778c76bc32e5e757b425
Zachary Rosen <zrosen97@gmail.com>
2025-07-29 15:31:16 -0400

    Add modeling_api

---
+++ modeling_api/plumber.R
@@ -0,0 +1,99 @@
+#
+# This is a Plumber API. You can run the API by clicking
+# the 'Run API' button above.
+#
+# Find out more about building APIs with Plumber here:
+#
+#    https://www.rplumber.io/
+#
+
+set.seed(123)
+library(plumber)
+library(tidyverse)
+library(rsample)
+library(caret)
+library(yardstick)
+
+diabetes <- read_csv("../data/diabetes_binary_health_indicators_BRFSS2015.csv")
+
+diabetes <- diabetes |>
+  mutate(Diabetes_binary = factor(Diabetes_binary, labels = c("N", "Y")),
+         HighBP = factor(HighBP),
+         HighChol = factor(HighChol),
+         Smoker = factor(Smoker),
+         PhysActivity = factor(PhysActivity),
+         Age = factor(Age, ordered = TRUE)
+  )
+
+mean_bmi <- mean(diabetes$BMI)
+
+mode_age <- names(sort(table(diabetes$Age), decreasing = TRUE))[1]
+mode_highBP <- names(sort(table(diabetes$HighBP), decreasing = TRUE))[1]
+mode_highChol <- names(sort(table(diabetes$HighChol), decreasing = TRUE))[1]
+mode_smoker <- names(sort(table(diabetes$Smoker), decreasing = TRUE))[1]
+mode_physActivity <- names(sort(table(diabetes$PhysActivity), decreasing = TRUE))[1]
+
+diabetes <- diabetes |>
+  mutate(Diabetes_binary = factor(Diabetes_binary, labels = c("N", "Y")),
+         HighBP = factor(HighBP),
+         HighChol = factor(HighChol),
+         Smoker = factor(Smoker),
+         PhysActivity = factor(PhysActivity),
+         Age = factor(Age, ordered = TRUE)
+  )
+
+data_split <- initial_split(diabetes, prop = 0.7)
+train <- training(data_split)
+test <- testing(data_split)
+
+bestTune <- expand.grid(
+  alpha = c(0.5),
+  lambda = c(0.001)
+)
+
+model_final <- train(Diabetes_binary ~ HighBP + HighChol + Smoker 
+                    + PhysActivity + Age*BMI,
+                    data = train,
+                    metric = "logLoss",
+                    preProcess = c("center", "scale"),
+                    trControl = trainControl(method = "none", classProbs = TRUE),
+                    method = "glmnet",
+                    family = "binomial",
+                    tuneGrid = bestTune
+)
+
+#* @apiTitle Model API
+#* @apiDescription Model API allows you to predict using the final model.
+
+#* Predict using final model
+#* @param HighBP The HighBP predictor 
+#* @param HighChol The HighChol predictor
+#* @param Smoker The Smoker predictor
+#* @param PhysActivity The PhysActivity predictor
+#* @param Age The Age predictor
+#* @param BMI The BMI predictor
+#* @get /pred
+function(HighBP = mode_highBP, HighChol = mode_highChol, Smoker = mode_smoker, 
+         PhysActivity = mode_physActivity, Age = mode_age, BMI = mean_bmi) {
+    pred_data <- data.frame(
+      HighBP = factor(HighBP, levels = levels(diabetes$HighBP)),
+      HighChol = factor(HighChol, levels = levels(diabetes$HighChol)),
+      Smoker = factor(Smoker, levels = levels(diabetes$Smoker)),
+      PhysActivity = factor(PhysActivity, levels = levels(diabetes$PhysActivity)),
+      Age = factor(Age, levels = levels(diabetes$Age), ordered = TRUE),
+      BMI = as.numeric(BMI)
+    )
+    
+    prediction <- predict(model_final, newdata = pred_data, type = "prob")
+    return(prediction)
+}
+
+#* Get information about API
+#* @get /info
+function() {
+  list(msg = "Zachary Rosen - https://zbrosen2.github.io/FinalProject/")
+}
+
+# https://localhost:8000/pred
+# https://localhost:8000/pred?HighBP=0&HighChol=0&Smoker=0&PhysActivity=0&Age=9&BMI=30
+# https://localhost:8000/pred?HighBP=1&HighChol=1&Smoker=1&PhysActivity=0&Age=10&BMI=45
